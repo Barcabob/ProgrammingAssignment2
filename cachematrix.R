1a75a3287e19eccd55552216b4fc198acbe38957
Barcabob <Skuggan@Peters-MacBook-Pro.local>
2015-05-24 13:35:50 +0200

    Your message here

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,37 @@
-## Put comments here that give an overall description of what your
-## functions do
+## The overall purpose of these two function is to make sure that the sometime  
+## Time-consuming computation of finding the inverse of a matrix only is done once, and then saves in memory
 
-## Write a short comment describing this function
+## Below function creates a list of functions that can: set the value of the matrix, get the value of the matrix,
+## set the reverse of a matrix, get the reverse of a matrix
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  rev <- NULL
+  set <- function(y) {
+    x <<- y
+    rev <<- NULL
+  }
+  get <- function() x
+  set_rev_matrix <- function(solve) rev <<- solve
+  get_rev_matrix <- function() rev
+  list(set = set, 
+       get = get,
+       set_rev_matrix = set_rev_matrix,
+       get_rev_matrix = get_rev_matrix)
 }
 
 
-## Write a short comment describing this function
+## The below function calculates the the inverse of the matrix defined in the makeCacheMatrix function 
+## and stores the inverse via the set_rev_matrix if not the inverse already has been calculated. If the 
+## Inverse of the Matrix already has been calculated it gets the mean from the Cache instead of calculating it
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  rev <- x$get_rev_matrix()
+  if(!is.null(rev)) {
+    message("getting cached data")
+    return(rev)
+  }
+  data <- x$get()
+  rev <- solve(data, ...)
+  x$set_rev_matrix(rev)
+  rev
 }
